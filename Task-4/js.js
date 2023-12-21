f66619a2e1c57016482642313ea875c11ff91a84
Amar Yasser <128907609+Amarr15@users.noreply.github.com>
2023-12-21 18:32:26 +0200

    Add files via upload

---
+++ Task-4/js.js
@@ -0,0 +1,65 @@
+
+
+//التكليف 01
+
+console.log(10 * 20 + 15 % 3 + 190 + 10 - 400);
+
+
+
+
+
+//التكليف 02
+
+let num = 3;
+
+// Solution One
+console.log(num+num); // 6
+
+// Solution Two
+console.log(num+true+true+true); // 6
+
+// Soultion Three
+console.log(num-- + ++num); // 6
+
+// Soultion Four
+console.log(true+true+true+true+true+true); // 6
+
+// Solution Five
+console.log(num*num-num); // 6
+
+// Solution Six
+console.log(num+=num); // 6
+
+
+
+//التكليف 03
+
+
+let x = "10";
+
+// Solution One
+console.log(+x + +x); // 20
+
+// Solution Two
+console.log(Number(x)+ +x); // 20
+
+// Solution Three
+console.log(+x +true+true+true+true+true+true+true+true+true+true); // 20
+
+// Solution Four
+console.log(+(++x) + +(++x) - +(--x) + +(++x) - +(--x) + +(--x) - +(--x) + +(--x) - +(--x) + +(--x) - +(--x) + +(--x)); // 20
+
+
+
+//التكليف 04
+
+
+let points = 10;
+
+// Write Your Code Here
+
+console.log(points+true+true+true); // 13
+
+// Write Your Code Here
+
+console.log(points - (--points - --points) - true); // 8;
